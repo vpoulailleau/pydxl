4cb29b92d3ac5b93819016450960e5befb0ef28a
Vincent Poulailleau <vpoulailleau@gmail.com>
2019-03-18 19:08:33 +0100

    fix version

--- pydxl/__init__.py
+++ pydxl/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Vincent Poulailleau"""
 __email__ = "vpoulailleau@gmail.com"
-__version__ = "__version__ = "2019.03.18""
+__version__ = "2019.03.18"
 
 from pydxl.exceptions import (
     DynamixelCommunicationException,
