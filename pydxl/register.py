3b18fd6564f5722de245875944b0f43c5881e1fd
Vincent Poulailleau <vpoulailleau@gmail.com>
2019-03-19 10:30:14 +0100

    listen to linter

--- pydxl/register.py
+++ pydxl/register.py
@@ -1,11 +1,18 @@
+"""Descriptor for a register."""
+
+
 class Register:
+    """Descriptor for a register."""
+
     def __init__(self, address=0, read=True, write=False, size=1):
+        """Initializer."""
         self.address = address
         self.read = read
         self.write = write
         self.size = size
 
     def __repr__(self):
+        """Represent a string."""
         if self.write:
             if self.read:
                 rw = "R/W"
@@ -16,6 +23,7 @@ class Register:
         return f"Reg(@{self.address}, {rw}, size: {self.size})"
 
     def __get__(self, instance, owner):
+        """Getter."""
         # instance.attr
         if instance:
             return instance.read(self.address, self.size)
@@ -24,8 +32,10 @@ class Register:
             return self
 
     def __set__(self, instance, value):
+        """Setter."""
         if self.write:
             instance.write(self.address, value, self.size)
 
     def __delete__(self, instance):
+        """Deleter. Does nothing."""
         pass
